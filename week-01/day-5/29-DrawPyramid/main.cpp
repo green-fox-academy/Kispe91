0c0049e66ca418adbb7fd56cc046b02f41513131
Kispe91 <marton.porkolab91@gmail.com>
2019-01-18 17:10:51 +0100

    DrawPyramid

--- week-01/day-5/29-DrawPyramid/main.cpp
+++ week-01/day-5/29-DrawPyramid/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int main(int argc, char* args[]) {
+
+    int cycleVariable=0;
+    int randomNumber;
+    std::string starString ="*";
+    std::cout << "Please give me a number " << std::endl;
+    std::cin >> randomNumber;
+    for (int i = 0; i < randomNumber; ++i) {
+        cycleVariable++;
+        for (int j = cycleVariable; j < randomNumber ; ++j) {
+            std::cout << " ";
+        }
+        std::cout << starString << std::endl;
+        starString = starString + "*" + "*";
+    }
+
     return 0;
 }
