7b6b5c3df2edce3c7daa89a90fdd6ee73f051278
Kispe91 <marton.porkolab91@gmail.com>
2019-01-21 16:51:52 +0100

    todoprint

--- week-02/day-1/todoprint/main.cpp
+++ week-02/day-1/todoprint/main.cpp
@@ -4,10 +4,10 @@
 int main(int argc, char* args[])
 {
     std::string todoText = " - Buy milk\n";
-    todoText.insert(0 , "My todo:\n");
+    todoText.insert(0, "My todo:\n");
     todoText.insert(todoText.length(), " - Download games\n");
     todoText.insert(todoText.length(), "\t - Diablo");
-
+   
     std::cout << todoText << std::endl;
 
     return 0;
