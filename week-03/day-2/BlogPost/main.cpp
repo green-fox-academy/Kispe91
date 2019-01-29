309ecba9ed3e838a0d9b7b90967fa9c5fd9b75a0
Kispe91 <marton.porkolab91@gmail.com>
2019-01-29 15:16:55 +0100

    BlogPost without header file

---
+++ week-03/day-2/BlogPost/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+class BlogPost {
+
+public:
+    std::string authorName;
+    std::string title;
+    std::string text;
+    std::string publicationDate;
+
+    BlogPost(std::string authorN, std::string tit, std::string txt, std::string pubDate) {
+        authorN = authorName;
+        tit = title;
+        txt = text;
+        pubDate = publicationDate;
+
+    }
+};
+int main() {
+
+    BlogPost blogPost1("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet", "2000.05.04.");
+    BlogPost blogPost2("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
+    BlogPost blogPost3("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28.");
+    return 0;
+}
