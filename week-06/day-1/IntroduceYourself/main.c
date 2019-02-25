2574b53690926039deb6c84510c86c494d128b8c
Kispe91 <marton.porkolab91@gmail.com>
2019-02-25 15:05:51 +0100

    IntroduceYourself

---
+++ week-06/day-1/IntroduceYourself/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    char * firstName = "Marton";
+    char * secondName = "Porkolab";
+    int age = 27;
+    float height = 1.86;
+
+    printf("%s %s\n%d\n%.2f\n", firstName, secondName, age, height);
+
+    return 0;
+}
